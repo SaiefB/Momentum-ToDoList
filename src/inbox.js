2027961281602053e45e042d2cfe7c3a0098b4ca
Saief <A.S.Bokor@outlook.com>
2025-01-13 21:47:49 +0000

    dynamically created the html in the content section

--- src/inbox.js
+++ src/inbox.js
@@ -1,13 +1,59 @@
 function generateInbox() {
 
-    const contentDiv = document.getElementById("#contentDiv");
+    const contentDiv = document.getElementById("contentDiv");
 
+    contentDiv.innerHTML = "";
+
+    // Inbox Title
     const inboxTitle = document.createElement("h1");
     inboxTitle.textContent = "Inbox";
-    contentDiv.appendChild(inboxTitle)
+    contentDiv.appendChild(inboxTitle);
+
+
+    // Button
+    const inboxButton = document.createElement("button");
+    inboxButton.className = "contentButton";
+    contentDiv.appendChild(inboxButton);
+
+    const contentButtonLeftDiv = document.createElement("div");
+    contentButtonLeftDiv.className = "contentButtonLeftDiv";
+    inboxButton.appendChild(contentButtonLeftDiv);
+
+    const inboxButtonCircle = document.createElement("i");
+    inboxButtonCircle.className = "fa-regular fa-circle"
+    contentButtonLeftDiv.appendChild(inboxButtonCircle);
+
+    const inboxButtonTask = document.createElement("p");
+    inboxButtonTask.textContent = "Complete Work";
+    contentButtonLeftDiv.appendChild(inboxButtonTask);
+
+    const contentButtonRightDiv = document.createElement("div");
+    contentButtonRightDiv.className = "contentButtonRightDiv";
+    inboxButton.appendChild(contentButtonRightDiv);
+
+    const inboxButtonDate = document.createElement("p");
+    inboxButtonDate.textContent = "13/01/24";
+    contentButtonRightDiv.appendChild(inboxButtonDate)
+
+    const inboxButtonBin = document.createElement("i");
+    inboxButtonBin.className = "fa-solid fa-trash"
+    contentButtonRightDiv.appendChild(inboxButtonBin);
+
+    //Add  Button
+    const addButton = document.createElement("button");
+    addButton.className = "contentAddButton";
+    contentDiv.appendChild(addButton);
+
+    const addButtonSymbol = document.createElement("i");
+    addButtonSymbol.className = "fa-solid fa-plus";
+    addButton.appendChild(addButtonSymbol);
+
+
 
 }
 
-export {generateInbox};
+export {
+    generateInbox
+};
 
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 
-import {generateInbox} from "./inbox"
+import {generateInbox} from "./inbox";
 
-generateInbox()
-alert("js working")
+
+generateInbox();
+
