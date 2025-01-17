8d394f4b6f47d367454a2df06f437c31b76f6b39
Saief <A.S.Bokor@outlook.com>
2025-01-17 13:31:14 +0000

    restructured HTML file and added some js elements

--- src/logic.js
+++ src/logic.js
@@ -22,5 +22,29 @@ class Task {
 };
 
 function validateForm() {
-    let taskTitle = 0;
+    const taskInput = document.querySelector("task");
+    const descriptionInput = document.querySelector(".description");
+    const dateInput = document.querySelector(".date");
+    const priorityInput = document.querySelector(".priority")
+
+    // If any field is empty, alert the user and return false to prevent submission.
+    if (taskInput === "" || descriptionInput === "" || dateInput === "" || priorityInput === "") {
+        alert("Please complete all required fields");
+        return false; // Form is not valid.
+    } else {
+        return true; // Form is valid.
+    }
+}
+
+const submitBtn = querySelector(".submit")
+
+/* -------------------- Button Functionality --------------------*/
+submitBtn.onclick = function(event) {
+    
+
+    event.preventDefault(); // Prevents the form from submitting and refreshing the page.
+    
+    if (validateForm()) {
+        console.log("success"); // Calls your function to add a book if the form is valid.
+    }
 }
