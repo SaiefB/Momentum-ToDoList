566a8a7b70de0d5b946a696cfd63250089c9fbf8
Saief <A.S.Bokor@outlook.com>
2025-02-12 11:10:58 +0000

    added debugging console.logs to index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./styles.css";
 import { submitButton, closeButton, cancelButton, addTaskButton, deleteTaskFunction, todaySectionFunction, inboxSectionFunction, weekSectionFunction, addNewProject, closeNewProject, confirmNewProject, displayProjects} from "./DOM";
 
 document.addEventListener("DOMContentLoaded", () => {
+    console.log("DOMContentLoaded and functions initialised");
     submitButton();
     closeButton();
     cancelButton();
